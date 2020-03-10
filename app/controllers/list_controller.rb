258fdb780d268cf4bf7f09dcbccdd8cd1c923bd2
ShimadaRikuya <59195586+ShimadaRikuya@users.noreply.github.com>
2020-03-10 17:50:40 +0900

    削除機能

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,6 +1,6 @@
 class ListController < ApplicationController
 
-before_action :set_list, only: %i(edit update)
+before_action :set_list, only: %i(edit update　destroy)
 
   def new
     @list = List.new
@@ -24,6 +24,11 @@ before_action :set_list, only: %i(edit update)
     else
       render action :edit
     end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to :root
   end
 
   private
